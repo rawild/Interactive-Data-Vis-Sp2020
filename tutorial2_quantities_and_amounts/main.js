09d276a3c5417a04b63223074e7c0cde73f3a23b
cw2sid <annalisa.wilde.test@gmail.com>
2020-02-12 17:19:06 -0500

    bar graph vertical complete

---
+++ tutorial2_quantities_and_amounts/main.js
@@ -0,0 +1,59 @@
+/* Load Data */
+d3.csv("../data/US_Wealth_Inequality_2016.csv", d3.autoType).then(data => {
+    console.log(data)
+
+    /* Constants in SVG Frame */
+    const width = window.innerWidth * 0.9,
+    height = window.innerHeight / 1.5,
+    paddingInner = 0.2,
+    margin = { top: 40, bottom: 40, left: 40, right:40 },
+    wealth = 98;
+
+    /* Scales for visualization */
+    const xScale = d3
+        .scaleBand()
+        .domain(data.map(d => d["Wealth Bracket"]))
+        .range([margin.left, width - margin.right])
+        .paddingInner(paddingInner);
+    const yScale = d3
+        .scaleLinear()
+        .domain(d3.extent(data, d => d.Percentage * wealth))
+        .range([height - margin.bottom, margin.top]);
+    const xAxis = d3.axisBottom(xScale).ticks(data.length);
+    
+    /* Shape Drawing Code */
+    // main svg square
+    const svg = d3
+        .select("#d3-container")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+    //append rects
+    const rect = svg
+        .selectAll("rect")
+        .data(data)
+        .join("rect")
+        .attr("x", d => xScale(d["Wealth Bracket"]))
+        .attr("y", d => yScale(Math.max(0,d.Percentage * wealth)))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => Math.abs(yScale(d.Percentage * wealth)-yScale(0)))
+        .attr("fill", "grey")
+    //append labels
+    const text = svg
+        .selectAll("text")
+        .data(data)
+        .join("text")
+        .attr("class", "label")
+        .attr("x", d => xScale(d["Wealth Bracket"]) + (xScale.bandwidth() / 2))
+        .attr("y", d => yScale(d.Percentage * wealth) - 40)
+        .text(d => "$"+ d.Percentage * wealth + " Billion")
+        .attr("dy", "1.25em")
+    // append X axis
+    svg
+        .append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(0,${yScale(0)})`)
+        .call(xAxis);
+    
+
+});
