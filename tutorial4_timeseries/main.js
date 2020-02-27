6189b9fde94300459ea3ecf77b435ef9ee729d05
cw2sid <annalisa.wilde.test@gmail.com>
2020-02-27 14:14:33 -0500

    stacked area achieved

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -3,7 +3,8 @@ const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
   radius = 5,
-  default_selection = "Select a Class";
+  default_selection = "Select a Class",
+  classes= ['Top 1%','Next 4%','Next 5%','Next 10%','Upper-Middle 20%','Middle 20%','Middle 20%','Lower-Middle 20%','Lowest 20%'];
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
@@ -11,25 +12,33 @@ let svg;
 let xScale;
 let yScale;
 let colorScale;
-let classes;
 let colors;
 
 /* APPLICATION STATE */
 let state = {
   data: [],
+  rawData: [],
   selection: null, 
   oldSelection: null
 };
 
 /* LOAD DATA */
 // + SET YOUR DATA PATH
-d3.csv('../data/US_income_inequality_removed_index.csv', d => ({
+d3.csv('../data/US_income_inequality.csv', d => ({
   year: new Date(d.Year, 0, 1),
-  class: d.Class,
-  percentWealth: parseFloat(d.WealthPercent)
-})).then(raw_data => {
-  console.log("raw_data", raw_data);
-  state.data = raw_data;
+  'Top 1%': parseFloat(d['Top 1.0%']),
+  'Next 4%': parseFloat(d['Next 4.0%']),
+  'Next 5%': parseFloat(d['Next 5.0%']),
+  'Next 10%': parseFloat(d['Next 10.0%']),
+  'Upper-Middle 20%': parseFloat(d['Upper-Middle 20.0%']),
+  'Middle 20%': parseFloat(d['Middle 20.0%']),
+  'Lower-Middle 20%': parseFloat(d['Lower-Middle 20.0%']),
+  'Lowest 20%': parseFloat(d['Lowest 20%'])
+})).then(raw_data => {let stack = d3.stack().keys(classes).order(d3.stackOrderAscending)
+  state.rawData = raw_data
+
+  state.data = stack(raw_data)
+  console.log(state.data)
   init();
 });
 
@@ -39,19 +48,18 @@ function init() {
   // + SCALES
   xScale = d3
     .scaleTime()
-    .domain(d3.extent(state.data, d => d.year))
+    .domain(d3.extent(state.rawData, d => d.year))
     .range([margin.left, width - margin.right]);
 
   yScale = d3
     .scaleLinear()
-    .domain(d3.extent(state.data, d => d.percentWealth))
+    .domain([d3.min(state.rawData, d=>d['Lowest 20%']),100])
     .range([height - margin.bottom, margin.top]);
-    
+
     // + AXES
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale)
   // Coloration
-  classes = [...Array.from(new Set(state.data.map(d => d.class)))]
   colors = ['complement-4','complement-3','complement-2','complement-1','primary-1','primary-2','primary-3','primary-4']
   colorScale = d3
     .scaleOrdinal()
@@ -76,7 +84,7 @@ function init() {
     .data(
       classes) // + ADD DATA VALUES FOR DROPDOWN
     .join("option")
-    .attr("value", d => d.class)
+    .attr("value", d =>d)
     .text(d => d);
 
   // + SET SELECT ELEMENT'S DEFAULT VALUE (optional)
@@ -112,7 +120,7 @@ function init() {
     .attr("y", "50%")
     .attr("dx", "-3em")
     .attr("writing-mode", "vertical-rl")
-    .text("Percent overall wealth");
+    .text("Percent of Overall Wealth");
   draw(); // calls the draw function
 }
 
@@ -120,23 +128,24 @@ function init() {
 // we call this everytime there is an update to the data/state
 function draw() {
   // + FILTER DATA BASED ON STATE
-  let filteredData;
-  if (state.selection !== null) {
+  let filteredData = state.data
+  /*if (state.selection !== null) {
     filteredData = state.data.filter(d => d.class === state.selection);
   } 
-  fill = colorScale(state.selection)
-  console.log(fill)
+  */
+ 
+ 
   
   const areaFunc = d3
     .area()
-    .x(d=> xScale(d.year))
-    .y0(yScale(0))
-    .y1(d => yScale(d.percentWealth))
+    .x(d=> xScale(d.data.year))
+    .y0(d => yScale(d[0]))
+    .y1(d => yScale(d[1]))
     
-  
-  const dot = svg
+  console.log(filteredData)
+  /*const dot = svg
   .selectAll(".dot")
-  .data(filteredData, d => d.year) // use `d.year` as the `key` to match between HTML and data elements
+  .data(filteredData, d => d.key) // use `d.year` as the `key` to match between HTML and data elements
   .join(
     enter =>
       // enter selections -- all data elements that don't have a `.dot` element attached to them yet
@@ -145,7 +154,7 @@ function draw() {
         .attr("class", "dot "+ fill) // Note: this is important so we can identify it in future updates
         .attr("r", radius)
         .attr("cy", height - margin.bottom) // initial value - to be transitioned
-        .attr("cx", d => xScale(d.year)),
+        .attr("cx", d => xScale(d.data.year)),
     update => update
     .attr("class", "dot "+ fill),
     exit =>
@@ -153,7 +162,7 @@ function draw() {
         // exit selections -- all the `.dot` element that no longer match to HTML elements
         exit
           .transition()
-          .delay(d => d.year)
+          .delay(d => d.data.year)
           .duration(500)
           .attr("cy", height - margin.bottom)
           .remove()
@@ -166,30 +175,19 @@ function draw() {
       selection
         .transition() // initialize transition
         .duration(1000) // duration 1000ms / 1s
-        .attr("cy", d => yScale(d.percentWealth)) // started from the bottom, now we're here
-  );
+        .attr("cy", d => yScale(d[1])) // started from the bottom, now we're here
+  );*/
 
   const area = svg
     .selectAll("path.area")
-    .data([filteredData], d=>d.year)
-    .join(
-      enter =>
-        enter
-          .append("path")
-          .attr("class",  "area " + fill)
-          .attr("opacity", 0), // start them off as opacity 0 and fade them in
-      update => update
-      .attr("class", "area "+ fill),
-         // pass through the update selection
-      exit => exit.remove()
-    )
-    .call(selection =>
-      selection
-        .transition() // sets the transition on the 'Enter' + 'Update' selections together.
-        .duration(1000)
-        .attr("opacity", .7)
-        .attr("d", d => areaFunc(d))
+    .data(filteredData, d=>d.key)
+    .join("path")
+        .attr("class",  d=> "area " + colorScale(d.key))
+        .attr("opacity", 0.7) // start them off as opacity 0 and fade them in
+        .attr("d", areaFunc)
         
-    );
+  
 }
+
+
 
