4f053a313c6c49c2fd7b649651af865a4987d3a6
cw2sid <annalisa.wilde@gmail.com>
2020-03-12 17:01:26 -0400

    treemap is working

--- tutorial6_hierarchical/main.js
+++ tutorial6_hierarchical/main.js
@@ -6,20 +6,20 @@ const width = window.innerWidth * 0.9,
   margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 let svg;
-let tooltip;
 
 /**
  * APPLICATION STATE
  * */
 let state = {
-  // + INITIALIZE STATE
+  root: null,
 };
 
 /**
  * LOAD DATA
  * */
-d3.json("../../data/flare.json", d3.autotype).then(data => {
+d3.csv("../data/senate_25_contributions_Jan_2020.csv", d3.autotype).then(data => {
   state.data = data;
+  console.log(data)
   init();
 });
 
@@ -30,28 +30,104 @@ d3.json("../../data/flare.json", d3.autotype).then(data => {
 function init() {
   const container = d3.select("#d3-container").style("position", "relative");
 
+  tooltip = container
+    .append("div")
+    .attr("width", 100)
+    .attr("height", 100)
+    .style("position", "absolute")
+    .style("background-color", "white");
+
   svg = container
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-  // + INITIALIZE TOOLTIP IN YOUR CONTAINER ELEMENT
+  const uniqueCandidates = [...new Set(state.data.map(d => d.Candidate))];
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(uniqueCandidates)
+    .range(d3.schemeCategory10);
 
-  // + CREATE YOUR ROOT HIERARCHY NODE
+  const rolledUp = d3.rollups(
+    state.data,
+    v => ({ total: d3.sum(v, d => d['AMOUNT_70']), donors: v }), // reduce function,
+    d => d.Candidate,
+    d => d.E_YEAR,
+    d => d.NAME
+  );
 
-  // + CREATE YOUR LAYOUT GENERATOR
+  console.log("rolledUp", rolledUp);
 
-  // + CALL YOUR LAYOUT FUNCTION ON YOUR ROOT DATA
+  // groups the data by genre, type and rating
+  // make hierarchy
+  const root = d3
+    .hierarchy([null, rolledUp], ([key, values]) => values) // children accessor, tell it to grab the second element
+    .sum(([key, values]) => values.total) // sets the 'value' of each level
+    .sort((a, b) => b.value - a.value);
 
-  // + CREATE YOUR GRAPHICAL ELEMENTS
+  // make treemap layout generator
+  const tree = d3
+    .treemap()
+    .size([width, height])
+    .padding(1)
+    .round(true);
+
+  // call our generator on our root hierarchy node
+  tree(root); // creates our coordinates and dimensions based on the heirarchy and tiling algorithm
+
+  console.log(root);
+
+  // create g for each leaf
+  const leaf = svg
+    .selectAll("g")
+    .data(root.leaves())
+    .join("g")
+    .attr("transform", d => `translate(${d.x0},${d.y0})`);
+
+  leaf
+    .append("rect")
+    .attr("fill-opacity", 0.6)
+    .attr("fill", d => colorScale(d.data[1].donors[0].Candidate)) // take the genre from the first one in the group
+    .attr("width", d => d.x1 - d.x0)
+    .attr("height", d => d.y1 - d.y0)
+    .on("mouseover", d => {
+      console.log("d", d);
+      state.hover = {
+        translate: [
+          // center tooltip in rect
+          d.x0 + (d.x1 - d.x0) / 2,
+          d.y0 + (d.y1 - d.y0) / 2,
+        ],
+        name: d
+          .ancestors()
+          .reverse()
+          .map(d => d.data[0])
+          .join("/"),
+        value: d.value,
+      };
+      draw();
+    });
 
   draw(); // calls the draw function
 }
-
 /**
  * DRAW FUNCTION
  * we call this everytime there is an update to the data/state
  * */
 function draw() {
-  // + UPDATE TOOLTIP
+  if (state.hover) {
+    tooltip
+      .html(
+        `
+        <div>Name: ${state.hover.name}</div>
+        <div>Value: ${state.hover.value}</div>
+      `
+      )
+      .transition()
+      .duration(500)
+      .style(
+        "transform",
+        `translate(${state.hover.translate[0]}px,${state.hover.translate[1]}px)`
+      );
+  }
 }
